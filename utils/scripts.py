43226162e9b3d19f2bd9539d2c8626ee570cac06
w-nicole <wnicole1549@gmail.com>
2021-07-14 17:22:31 -0700

    joint submission

--- utils/scripts.py
+++ utils/scripts.py
@@ -12,23 +12,22 @@ def gen_singularity_header(om2_user = 'wongn'):
     return f"singularity exec --nv -B /om,/om2/user/{om2_user} /om2/user/{om2_user}/vagrant/trans-pytorch-gpu " 
     
 
-def gen_command_header(mem_alloc, time_alloc_hrs):
+def gen_command_header(mem_alloc_gb, time_alloc_hrs):
     
     commands = []
     commands.append("#!/bin/bash\n")
     
     # Citation text for every script
-    commands.append("\n# For the command text\n# 6/24/21: https://github.mit.edu/MGHPCC/OpenMind/wiki/How-to-use-Singularity-container%3F\n# and https://github.mit.edu/MGHPCC/OpenMind/issues/3392\n# including the bash line at the top\n")
+    commands.append("\n# For the command text\n# 6/24/21: https://github.mit.edu/MGHPCC/OpenMind/wiki/How-to-use-Singularity-container%3F\n# and https://github.mit.edu/MGHPCC/OpenMind/issues/3392\n# including the bash line at the top, and all but the python3 commands\n")
     
     commands.append("\n#SBATCH -N 1\n")                         
     commands.append("#SBATCH -p cpl\n")
     commands.append("#SBATCH --gres=gpu:1\n")
     commands.append(f"#SBATCH -t {time_alloc_hrs}:00:00\n")
-    commands.append(f"#SBATCH --mem={mem_alloc}G\n")
+    commands.append(f"#SBATCH --mem={mem_alloc_gb}G\n")
     commands.append("#SBATCH --constraint=high-capacity\n")
      
     commands.append("\nmodule load openmind/singularity/3.2.0\n")
-    commands.append("# end taken code")
     
     return commands
 

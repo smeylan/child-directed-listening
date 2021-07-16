5d62b5951d8d6e94d9a72278fae36274934deb21
w-nicole <wnicole1549@gmail.com>
2021-07-15 20:01:04 -0700

    adding in progress child code so om will run

---
+++ utils_child/child_models.py
@@ -0,0 +1,51 @@
+
+import os
+from os.path import join, exists
+import json
+import shutil
+
+from utils import load_models, load_splits
+import config
+
+
+def get_best_child_base_model_path(which_metric = 'perplexity'):
+    """
+    Between the CHILDES no tags and with tags all/all models, choose the one with lower val perplexity.
+    """
+    
+    assert which_metric in {'perplexity', 'eval_loss'}
+    
+    with_tags_path = load_models.get_model_path('all', 'all', False)
+    no_tags_path = load_models.get_model_path('all', 'all', True)
+    
+    which_result = {} 
+    for tags_str, tags_path in zip(['with', 'no'], [with_tags_path, no_tags_path]):
+        with open(join(tags_path, 'all_results.json'), 'r') as f:
+            which_results[tags_str] = json.load(f)[which_metric]
+    
+    if which_results['with'] >= which_results['no']:
+        return with_tags_path, True
+    else:
+        return no_tags_path, False
+
+def get_child_names():
+    """
+    Get all Providence children.
+    """
+    
+    all_phono = load_splits.load_eval_data_all('all', 'all')['phono']
+    return set(all_phono.target_child_name)
+    
+    
+def get_child_model_dict(split, dataset, with_tags):
+    
+    
+    # But you actually need to path to the model
+    base_model_path, is_tags = get_best_child_base_model_path()
+    base_model_dict = get_model_from_path(base_model_path, is_tags)
+    
+    # Need to copy the appropriate model to all folders in preparation for finetuning.
+    
+    return base_model_dict
+    
+    pass
---
+++ utils_child/utils_child.py
@@ -0,0 +1,74 @@
+
+
+from utils import load_splits
+from utils_model_sampling import beta_utils
+
+import config
+
+def find_best_model():
+    
+    # Need to compare the with_tags and no_tags models, choose the one with lower perplexity,
+    # then copy it for finetuning automatically to the right child folder.
+    
+    # Then, need to prep scripts for child finetuning
+    
+    # Then, need to score one child's productions via another child's -- but how?
+    
+    # for the priors/likelihoods:
+    # for one child
+    # do this: compare_successes_failures -> select on all utterance ids
+    # then this: get_edit_distance_matrix
+    
+    
+    # then, you should somehow score the two separately -- but it's not obvious what the partition in the data is.
+    
+    
+    
+def score_cross_prior(data_child, prior_child):
+    
+    """
+    Calculate one child's posterior distr. on their data
+        with the prior of another child.
+    """
+    
+    
+    # For the priors/etc you actually need to?
+    # Note for the run across beta etc. you need to...
+    # You need to load the entirety of the child data to perform the optimization
+    # So it may be different than the current code.
+    
+    # Specifically for load_sample_successes, you will just have to recopy all the data to that location
+    # (optimize on the entire split)
+    
+    # Load the proper beta value for the given child.
+    
+    # How to load the right model dictionary here? 
+    
+    optim_beta = beta_utils.get_optimal_beta_value('child', prior_child, True, 0, 'childes')
+    print('Update this to use best model, not just speaker tags/no context')
+    
+    # You should load the better model here -> use the config files to decide if you can.
+    
+    # Load the evaluation successes and yyy for a given child.
+    eval_data = load_splits.load_child_eval_data(data_child)
+    # prior_model = load_models.
+    
+    # Pass the evaluation set to scoring 
+    # What to load for all_tokens_phono?
+    # You need to separate yyy, all, and success for the eval set itself.
+    # How to avoid confusing naming conventions?
+    
+    # Use id, not utterance id, because this is Providence second query data.
+    priors = compare_successes_failures(eval_data['phono'], eval_data['success_utts'].id, eval_data['yyy_utts'].id, **model['kwargs'])
+    
+    # This will need to be 
+    
+    
+    
+    
+    
+    
+    # Load the success and yyy for a given child.
+    
+def 
+    

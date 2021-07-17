094470367fdb772ac76647feb480c05666390e27
w-nicole <wnicole1549@gmail.com>
2021-07-16 19:53:58 -0700

    trying to develop on om

--- debug_find_example.py
+++ debug_find_example.py
@@ -1,7 +1,7 @@
 # Finding a problematic sample on OM -- debug purposes only.
 
 from utils import load_splits, load_models
-import sample_models_across_time 
+from utils_model_sampling import sample_models_across_time 
 
 if __name__ == '__main__':
 
@@ -10,9 +10,15 @@ if __name__ == '__main__':
 
     # Load the successes
     this_age = 4.0
-    this_pool = load_splits.load_sample_successes('models_across_time', 'all', 'all', age = this_age)
+    this_pool_ids = load_splits.load_sample_successes('models_across_time', 'all', 'all', age = this_age)
 
-    tokens = load_splits.load_eval_data_all('all', 'all')['phono']
+    # Need to index into this pool? How to do so? Make a function to do this?
+    
+    eval_data = load_splits.load_eval_data_all('all', 'all')
+    tokens = eval_data['phono']
+    all_pool = pd.concat([eval_data['success_utts'], eval_data['yyy_utts']])
+    
+    this_pool = all_pool[all_pool.utterance_id.isin(this_pool_ids)]
     
     # Use any beta for now because this is just debugging.
     this_scores = sample_models_across_time.successes_and_failures_across_time_per_model(this_age, this_pool, this_model_dict, tokens, beta_value = 3.2)

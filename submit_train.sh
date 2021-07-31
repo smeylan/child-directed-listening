229bed4b21412fec45b44d761326cf5fa9e83594
w-nicole <wnicole1549@gmail.com>
2021-07-30 19:35:26 -0700

    submit train file

--- submit_train.sh
+++ submit_train.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
 # All of the text in this, including the header and comment above, are from
 # 7/2/21: https://www.cyberciti.biz/faq/bash-loop-over-file/
-FILES="./scripts_random_seed/*"
+FILES="./scripts_beta_search/*"
 for f in $FILES
 do
   echo "Processing $f file..."

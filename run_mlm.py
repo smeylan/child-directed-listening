f15cf66f5287fa607ccb7a11340b926127699bb6
w-nicole <wnicole1549@gmail.com>
2021-08-01 15:39:20 -0700

    still same as before but change number of checkpoints saved

--- run_mlm.py
+++ run_mlm.py
@@ -446,6 +446,9 @@ def main():
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
         tokenizer=tokenizer,
+        # 8/1/21: modified
+        save_total_limit=1,
+        # end modifications
         data_collator=data_collator,
     )
 

ac3de46824a95af7df6f0a66f706ad085dc5f78e
w-nicole <wnicole1549@gmail.com>
2021-08-02 07:54:55 -0700

    version used to generate the data splits.

--- submit_train.sh
+++ submit_train.sh
@@ -1,7 +1,25 @@
 # #!/bin/bash
+
+# For the command text
+# 6/24/21: https://github.mit.edu/MGHPCC/OpenMind/wiki/How-to-use-Singularity-container%3F
+# and https://github.mit.edu/MGHPCC/OpenMind/issues/3392
+# including the bash line at the top, and all but the python3 commands
+
+#SBATCH -N 1
+#SBATCH -p cpl
+#SBATCH -t 00:10:00
+#SBATCH --mem=50M
+
+module load openmind/singularity/3.2.0
+rm -r ./experiments
+
+singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu python3 gen_training_scripts.py
+
 # NOTE : Quote it else use array to avoid problems #
-# All of the text in this, including the header and comment above, are from
+# All text below in this
 # 7/2/21: https://www.cyberciti.biz/faq/bash-loop-over-file/
+
+
 FILES="./scripts_train/*"
 for f in $FILES
 do

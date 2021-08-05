324041e90c78fee974e66c0908dc8840d2d4670d
w-nicole <wnicole1549@gmail.com>
2021-08-04 17:36:46 -0700

    child train/beta scripts

---
+++ scripts_child_train_beta/run_model_child_Naima_with_tags.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# For the command text
+# 6/24/21: https://github.mit.edu/MGHPCC/OpenMind/wiki/How-to-use-Singularity-container%3F
+# and https://github.mit.edu/MGHPCC/OpenMind/issues/3392
+# including the bash line at the top, and all but the python3 commands
+
+rsync -a --progress /om2/user/wongn/child-directed-listening/experiments/no_versioning/models/all/all/with_tags /om2/user/wongn/child-directed-listening/experiments/no_versioning/models/child/Naima/with_tags
+mv /om2/user/wongn/child-directed-listening/experiments/no_versioning/models/all/all/with_tags /om2/user/wongn/child-directed-listening/experiments/no_versioning/models/child/Naima/with_tags
+
+#SBATCH -N 1
+#SBATCH -p cpl
+#SBATCH --gres=gpu:1
+#SBATCH -t 02:05:00
+#SBATCH --mem=35G
+#SBATCH --constraint=high-capacity
+
+module load openmind/singularity/3.2.0
+mkdir ~/.cache/$SLURM_JOB_ID
+# 7/13/21: https://stackoverflow.com/questions/19960332/use-slurm-job-id for variable name of job ID
+singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu     python3 run_mlm.py             --model_name_or_path bert-base-uncased             --do_train             --do_eval             --output_dir /om2/user/wongn/child-directed-listening/experiments/no_versioning/models/child/Naima/with_tags            --train_file /om2/user/wongn/child-directed-listening/finetune/child/Naima/train.txt             --validation_file /om2/user/wongn/child-directed-listening/finetune/child/Naima/val.txt             --cache_dir ~/.cache/$SLURM_JOB_ID             --overwrite_output_dir; python3 run_beta_search.py --split child --dataset Naima --context_width 0 --use_tags True --model_type childes

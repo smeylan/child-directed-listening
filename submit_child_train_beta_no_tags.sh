0f0157d8c36c03b40dd7ea064382a289a65ef5e6
w-nicole <wnicole1549@gmail.com>
2021-08-12 10:58:07 -0700

    forgot to add no_tags only run

---
+++ submit_child_train_beta_no_tags.sh
@@ -0,0 +1,11 @@
+# #!/bin/bash
+# NOTE : Quote it else use array to avoid problems #
+# All of the text in this, including the header and comment above, are from
+# 7/2/21: https://www.cyberciti.biz/faq/bash-loop-over-file/
+FILES="./scripts_child_train_beta/no_tags/*"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  sbatch $f
+  cat "$f"
+done

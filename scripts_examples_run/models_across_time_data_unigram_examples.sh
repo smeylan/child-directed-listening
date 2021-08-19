8d78fb77223924a969e28cacc3a6c56cfabb9c1e
w-nicole <wnicole1549@gmail.com>
2021-08-19 11:01:17 -0700

    fixed examples gen script more

--- scripts_examples_run/models_across_time_data_unigram_examples.sh
+++ scripts_examples_run/models_across_time_data_unigram_examples.sh
@@ -15,4 +15,4 @@
 mkdir -p /om2/user/wongn/child-directed-listening/experiments/no_versioning/scores/n=500/val/all/all
 
 module load openmind/singularity/3.2.0
-singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu python3 run_beta_search.py --split all --dataset all --context_width 0 --use_tags False --model_type data_unigram; singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu  python3 run_models_across_time.py --split all --dataset all --context_width 0 --use_tags False --model_type data_unigram
+singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu python3 run_beta_search.py --split all --dataset all --context_width 0 --use_tags False --model_type data_unigram; singularity exec --nv -B /om,/om2/user/wongn /om2/user/wongn/vagrant/trans-pytorch-gpu  python3 run_models_across_time.py --split all --dataset all --context_width 0 --use_tags False --model_type data_unigram --examples_mode True

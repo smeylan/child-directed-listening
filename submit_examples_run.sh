75e6d1b5b6f47325f1381f515c2eaa9e1e5585b3
w-nicole <wnicole1549@gmail.com>
2021-08-19 14:18:46 -0700

    removing scripts

--- submit_examples_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p experiments/no_versioning/scores/n=500/val/all/all/
-FILES="./scripts_examples_run/*"
-for f in $FILES
-do
-	echo "Processing $f file..."
-	sbatch $f
-	cat "$f"
-done

4c2cbc2562116b3607f5f6d38d0e584b3bc1e8c6
w-nicole <wnicole1549@gmail.com>
2021-08-19 19:08:13 -0700

    correcting tier 2

--- tier_2.sh
+++ tier_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-chmod u+x /home/wongn/child_repo_split/tier_2a_non_child_scores_finetune_and_child_train.sh
-chmod u+x /home/wongn/child_repo_split/tier_2b_non_child_train_shelf_scores.sh
+chmod u+x tier_2a_non_child_train_shelf_scores.sh
+chmod u+x tier_2b_finetune_scores_child_train.sh
 
-./tier_2a_non_child_scores_finetune_and_child_train.sh
-./tier_2b_non_child_train_shelf_scores.sh
+tier_2a_non_child_train_shelf_scores.sh
+tier_2b_finetune_scores_child_train.sh
 
 # Then the following on Chompsky:
 # tmux attach-session -t experiments

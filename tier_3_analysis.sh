46e866ed28c55d67b8de56b87c7743349601ba4e
Stefan Meylan <smeylan@openmind7.ib.cluster>
2021-08-26 19:55:57 -0400

    fixed rsync destination so it doesn't produce experiments/experiments

--- tier_3_analysis.sh
+++ tier_3_analysis.sh
@@ -1,8 +1,8 @@
 
-rm 'Examples for Succcess and Failure Table.nbconvert.ipynb'
-rm 'Models across time analyses.nbconvert.ipynb'
-rm 'Child evaluations.nbconvert.ipynb'
-rm 'Prevalence analyses.nbconvert.ipynb'
+rm -rf 'Examples for Succcess and Failure Table.nbconvert.ipynb'
+rm -rf 'Models across time analyses.nbconvert.ipynb'
+rm -rf 'Child evaluations.nbconvert.ipynb'
+rm -rf 'Prevalence analyses.nbconvert.ipynb'
 
 jupyter nbconvert --execute 'Examples for Succcess and Failure Table' --to notebook
 jupyter nbconvert --execute 'Models across time analyses' --to notebook

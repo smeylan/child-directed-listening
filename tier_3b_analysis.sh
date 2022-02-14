6d490339c12bd82ac40a8ad451ab32677cb6d5c7
Stephan Meylan <meylan.stephan@gmail.com>
2022-02-14 07:11:30 -0800

    removed 4th notebook from tier 3

--- tier_3b_analysis.sh
+++ tier_3b_analysis.sh
@@ -1,10 +1,8 @@
-rm 'Examples for Succcess and Failure Table.nbconvert.ipynb'
-rm 'Models across time analyses.nbconvert.ipynb'
-rm 'Child evaluations.nbconvert.ipynb'
-rm 'Prevalence analyses.nbconvert.ipynb'
+rm -rf 'src/tier_3/Examples for Succcess and Failure Table.nbconvert.ipynb'
+rm -rf 'src/tier_3/Models across time analyses.nbconvert.ipynb'
+rm -rf 'src/tier_3/Child evaluations.nbconvert.ipynb'
+
 
 jupyter nbconvert --execute 'code/tier_3/1 - Examples for Success and Failure Table.ipynb' --to notebook
 jupyter nbconvert --execute 'code/tier_3/2 - Models Across Time Analyses.ipynb' --to notebook
 jupyter nbconvert --execute 'code/tier_3/3 - Child Evaluations.ipynb' --to notebook
-jupyter nbconvert --execute 'code/tier_3/4 - Prevalence Analyses.ipynb' --to notebook
-

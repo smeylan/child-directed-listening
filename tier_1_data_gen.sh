3412e0f5944c7bb99dae64756f8af87c3616b306
Nicole Wong <nicolewong@dhcp-10-31-115-60.dyn.MIT.EDU>
2022-11-19 15:03:43 -0500

    initial deletions

--- tier_1_data_gen.sh
+++ tier_1_data_gen.sh
@@ -6,13 +6,6 @@ rm *.nbconvert.ipynb
 
 # Run notebooks that prepare the data and generate the data splits
 jupyter nbconvert --execute 'src/tier_1/1 - Create Vocabulary and Pronunciations.ipynb' --to notebook
-jupyter nbconvert --execute 'src/tier_1/2 - Generalized Phonological Comparison' --to notebook
-jupyter nbconvert --execute 'src/tier_1/3 - Providence - Retrieve Data.ipynb' --to notebook
-jupyter nbconvert --execute 'src/tier_1/4 - Providence - Splits.ipynb' --to notebook
-jupyter nbconvert --execute 'src/tier_1/5 - Get Non-Providence CHILDES Finetuning Data.ipynb' --to notebook
-jupyter nbconvert --execute 'src/tier_1/6 - Output Citation vs. Observed Phonology Pairs.ipynb' --to notebook
-jupyter nbconvert --execute 'src/tier_1/7 - Data Splitting Checks.ipynb' --to notebook
-bash 'src/tier_1/8 - Train FST.sh' > fst_training.log
-
-# Then, rsync the resulting files to the SLURM cluster
-rsync -az --progress ./output ${SLURM_USERNAME}:${CDL_SLURM_ROOT}
+jupyter nbconvert --execute 'src/tier_1/2 - Providence - Retrieve Data.ipynb' --to notebook
+jupyter nbconvert --execute 'src/tier_1/3 - Providence - Splits.ipynb' --to notebook
+jupyter nbconvert --execute 'src/tier_1/4 - Get Non-Providence CHILDES Finetuning Data.ipynb' --to notebook
--- tier_3a_rsync.sh
+++
@@ -1,4 +0,0 @@
-# rsync results from SLURM back to the local machine
-# to be run on the local (non-SLURM) machine
-
-rsync -az --progress ${SLURM_USERNAME}:${CDL_SLURM_ROOT}/output .  
